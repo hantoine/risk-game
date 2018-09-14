484c8301df3912a639e9ff10e57cddfcdd37691a
Nelly Irahola <nellybett.irahola@gmail.com>
2018-09-13 19:03:22 -0500

    File Management Skeleton

---
+++ riskGame/src/com/risk/models/FileManagement.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.risk.models;
+
+/**
+ *
+ * @author Nellybett
+ */
+public class FileManagement {
+    
+    public static Board createBoard(String path){
+        return null;
+    }
+    
+    // New requirement - Discussion
+    public static Board generateBoardFile(String path){
+        return null;
+    }
+}
