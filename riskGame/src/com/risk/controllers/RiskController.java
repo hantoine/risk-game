300b95eacc34ea9857a5e06b8f17d5d23fe30b37
Nellybett <Nellybett@DESKTOP-7IID5TQ.wireless.concordia.ca>
2018-09-14 17:59:30 -0500

    Fix scroll=none problem

--- riskGame/src/com/risk/controllers/RiskController.java
+++ riskGame/src/com/risk/controllers/RiskController.java
@@ -20,7 +20,7 @@ public class RiskController {
     public RiskController(RiskView viewRisk, RiskModel modelRisk) {
         this.modelRisk = modelRisk;
         this.viewRisk = viewRisk;
-        modelRisk.setBoard("C:\\Users\\Nellybett\\Desktop\\World.txt");
+        modelRisk.setBoard("C:\\Users\\Nellybett\\Desktop\\Old Yorkshire.map");
         viewRisk.initialMap(modelRisk);
         this.viewRisk.setVisible(true);
        
---
+++ riskGame/src/com/risk/views/PlayerPanel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.risk.views;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Nellybett
+ */
+public class PlayerPanel extends JPanel{
+
+    public PlayerPanel() {
+    }
+}
