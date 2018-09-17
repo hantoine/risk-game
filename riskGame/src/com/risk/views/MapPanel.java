299eb3e4daf5388a053b751f8e3773244034d953
Antoine Hebert <hantoine@laptop-hantoine>
2018-09-17 00:20:39 -0400

    Add CountryButtons on MapPanel

--- riskGame/src/com/risk/views/MapPanel.java
+++ riskGame/src/com/risk/views/MapPanel.java
@@ -1,7 +1,10 @@
 package com.risk.views;
 
 import com.risk.models.Board;
+import com.risk.models.Country;
 import java.awt.Graphics;
+import java.util.Collection;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MapPanel extends JPanel {
@@ -9,8 +12,13 @@ public class MapPanel extends JPanel {
     private final Board board;
 
     public MapPanel(Board board) {
+        super(null);
         this.board = board;
         this.setSize(this.board.getImage().getWidth(null), this.board.getImage().getHeight(null));
+        Collection<Country> territories = board.getGraphTerritories().values();
+        territories.stream().forEach((currentCountry) -> {
+            this.add(new CountryButton(currentCountry));
+        });
     }
 
     @Override
