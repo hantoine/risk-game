ec43a622060afbb5a2725427cf423f3e385088ce
liyixuan <liyixuan4030614@gmail.com>
2018-09-17 09:15:19 -0400

    add a player information panel

---
+++ riskGame/src/com/risk/models/Hand.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.risk.models;
+
+/**
+ *
+ * @author l_yixu
+ */
+public class Hand {
+    // a group of cards
+    
+    private Card[] hand = new Card[5];
+    
+}
