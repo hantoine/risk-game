1b2f96096bd8ecbf29314f0ba6594845d5d3c471
Antoine Hebert <hantoine@laptop-hantoine>
2018-09-16 22:12:06 -0400

    Replace string manipulation by path manipulation in FileManagement.createBoard

--- riskGame/src/com/risk/models/FileManagement.java
+++ riskGame/src/com/risk/models/FileManagement.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.LinkedList;
 import javax.imageio.ImageIO;
@@ -28,13 +29,11 @@ public class FileManagement {
         HashMap<String, Continent> graphContinents = new HashMap();
         HashMap<String, Country> graphTerritories = new HashMap();
 
-        try {
-            File fileRead = new File(path);
+        File fileRead = new File(path);
 
-            //Use BufferedRead and FileReader
-            BufferedReader dataInput = new BufferedReader(new FileReader(fileRead));
-
-            //Reading the information
+        //Reading the information
+        //Use BufferedRead and FileReader
+        try (BufferedReader dataInput = new BufferedReader(new FileReader(fileRead))) {
             String linesRead;
             linesRead = dataInput.readLine();
 
@@ -47,23 +46,20 @@ public class FileManagement {
                         linesRead = dataInput.readLine();
                         while (!linesRead.equals("")) {
                             System.out.println("lineas leidas" + linesRead);
-                            aux = null;
                             aux = linesRead.split("=", 2);
                             System.out.println("Campo-valor: " + aux[0] + "," + aux[1]);
+                            //maybe we could use switch case here ? https://www.geeksforgeeks.org/string-in-switch-case-in-java/
+
                             if ("author".equals(aux[0])) {
                                 System.out.println("Entro");
                                 configurationInfo.put(aux[0], aux[1]);
-
                             } else if ("image".equals(aux[0])) {
-                                int pathPos = 0;
                                 configurationInfo.put(aux[0], aux[1]);
-                                pathPos = path.lastIndexOf("\\");
-                                BufferedImage image = ImageIO.read(new File(path.substring(0, pathPos) + "\\" + aux[1]));
+                                Path imagePath = fileRead.toPath().resolveSibling(aux[1]);
+                                BufferedImage image = ImageIO.read(new File(imagePath.toString()));
                                 board.setImage(image);
-
                             } else if (("wrap".equals(aux[0])) && (aux[1].equals("no") || aux[1].equals("yes"))) {
                                 configurationInfo.put(aux[0], aux[1]);
-
                             } else if (("scroll".equals(aux[0])) && (aux[1].equals("horizontal") || aux[1].equals("vertical") || aux[1].equals("none"))) {
                                 configurationInfo.put(aux[0], aux[1]);
                             } else if ((aux[0].equals("warn")) && (aux[1].equals("no") || aux[1].equals("yes"))) {
@@ -91,33 +87,34 @@ public class FileManagement {
                         System.out.println("File" + linesRead);
                         linesRead = dataInput.readLine();
                         while (linesRead != null) {
-                            if (!linesRead.equals("")) {
-                                aux = linesRead.split(",");
+                            if (linesRead.equals("")) {
+                                linesRead = dataInput.readLine();
+                                continue;
+                            }
 
-                                int i = 0;
-                                if (aux.length > 4) {
+                            aux = linesRead.split(",");
 
-                                    Country auxCountry = new Country(aux[0], Integer.parseInt(aux[1]), Integer.parseInt(aux[2]));
-                                    System.out.println("Country-----------" + aux[0]);
-                                    LinkedList<String> auxAdj = new LinkedList<>();
-                                    System.out.println("CREO EL PAIS");
-                                    for (i = 0; i < aux.length - 4; i++) {
-                                        System.out.println("Adj" + aux[i + 4]);
-                                        auxAdj.add(aux[i + 4]);
-                                    }
-                                    System.out.println("Continente al que pertenece " + aux[3]);
-                                    auxCountry.setAdj(auxAdj);
-                                    System.out.println("CREO las adj");
-
-                                    graphContinents.get(aux[3]).setMember(auxCountry);
-                                    System.out.println("Lo metio en un continente");
-
-                                    graphTerritories.put(aux[0], auxCountry);
-                                    System.out.println("Lo metio en los paises");
-                                } else {
-                                    throw new IOException();
+                            int i = 0;
+                            if (aux.length > 4) {
+                                Country auxCountry = new Country(aux[0], Integer.parseInt(aux[1]), Integer.parseInt(aux[2]));
+                                System.out.println("Country-----------" + aux[0]);
+                                LinkedList<String> auxAdj = new LinkedList<>();
+                                System.out.println("CREO EL PAIS");
+                                for (i = 0; i < aux.length - 4; i++) {
+                                    System.out.println("Adj" + aux[i + 4]);
+                                    auxAdj.add(aux[i + 4]);
                                 }
+                                System.out.println("Continente al que pertenece " + aux[3]);
+                                auxCountry.setAdj(auxAdj);
+                                System.out.println("CREO las adj");
 
+                                graphContinents.get(aux[3]).setMember(auxCountry);
+                                System.out.println("Lo metio en un continente");
+
+                                graphTerritories.put(aux[0], auxCountry);
+                                System.out.println("Lo metio en los paises");
+                            } else {
+                                throw new IOException();
                             }
 
                             linesRead = dataInput.readLine();
@@ -135,8 +132,6 @@ public class FileManagement {
             board.setConfigurationInfo(configurationInfo);
             board.setGraphTerritories(graphTerritories);
             board.setGraphContinents(graphContinents);
-            dataInput.close();
-
         } catch (FileNotFoundException e) {
             System.err.println("File not Found: " + e.getMessage());
         } catch (IOException e) {
