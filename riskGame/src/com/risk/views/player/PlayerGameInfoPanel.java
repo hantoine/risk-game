f6133c897311bddd94e5acb840eeae06df2969e3
Antoine Hebert <hantoine@laptop-hantoine>
2018-09-20 14:02:39 -0400

    Fix code formatting

--- riskGame/src/com/risk/views/player/PlayerGameInfoPanel.java
+++ riskGame/src/com/risk/views/player/PlayerGameInfoPanel.java
@@ -17,31 +17,29 @@ import javax.swing.JPanel;
  */
 public class PlayerGameInfoPanel extends JPanel {
 
-
     private Player currentPlayer;
     private JButton playerName;
     JLabel numArmies;
-    
+
     public PlayerGameInfoPanel(Player currentPlayer) {
-        this.currentPlayer=currentPlayer;
+        this.currentPlayer = currentPlayer;
         this.setLayout(new FlowLayout());
-        this.setSize(150,200);
-        this.playerName=new JButton("Player 1");
+        this.setSize(150, 200);
+        this.playerName = new JButton("Player 1");
         this.add(playerName);
-       
-        
+
     }
-    
-    public void updatePlayer(Player currentPlayer){
-        playerName.setText("Player "+currentPlayer.getName());
+
+    public void updatePlayer(Player currentPlayer) {
+        playerName.setText("Player " + currentPlayer.getName());
         playerName.setBackground(currentPlayer.getColor());
     }
-    
+
     public void setCurrentPlayer(Player currentPlayer) {
         this.currentPlayer = currentPlayer;
     }
-    
-        /**
+
+    /**
      * @return the playerName
      */
     public JButton getPlayerName() {
@@ -54,6 +52,5 @@ public class PlayerGameInfoPanel extends JPanel {
     public void setPlayerName(JButton playerName) {
         this.playerName = playerName;
     }
-    
 
 }
