15c2d440483d4fdedf9dfda16e9b4e23b65056c4
Nelly Irahola <nellybett.irahola@gmail.com>
2018-09-24 13:53:22 -0400

    refactoring

--- riskGame/src/com/risk/controllers/RiskController.java
+++ riskGame/src/com/risk/controllers/RiskController.java
@@ -50,21 +50,15 @@ public class RiskController implements ActionListener{
             JMenuItem source = (JMenuItem) c;
             System.out.println(source.getText());
             if(source.getText().equals("New Game")){
-                StartMenuView aux1=new StartMenuView(modelRisk, menuListener);
-                
-                MenuView aux=new MenuView(aux1,viewRisk, "New Game");
-                this.viewRisk.setMenuPanel(aux);
-                aux.add(aux1);
-                aux.setSize(300,500);
-                Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
-                aux.setLocation(dimension.width / 2 - 300 / 2, dimension.height / 2 - 500 / 2);
-                aux.setVisible(true);
+                viewRisk.initialMenu(modelRisk, menuListener);
             }
         }
     }
 
     void initGame() {
         
+        this.viewRisk.initialPlayer(modelRisk);
         this.viewRisk.initialMap(modelRisk, countryListener);
+        
     }
 }
