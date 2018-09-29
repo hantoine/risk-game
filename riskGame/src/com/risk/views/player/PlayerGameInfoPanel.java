83db620751135f9f92eb0364cd1270fc7a2587b3
liyixuan <liyixuan4030614@gmail.com>
2018-09-29 12:17:55 -0400

    Change the Player panel to display the total number of armies instead of the number of different types of armies

--- riskGame/src/com/risk/views/player/PlayerGameInfoPanel.java
+++ riskGame/src/com/risk/views/player/PlayerGameInfoPanel.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -20,6 +20,7 @@ import javax.swing.JPanel;
 
 /**
  * View of the player information
+ *
  * @author Will
  */
 public class PlayerGameInfoPanel extends JPanel {
@@ -34,91 +35,28 @@ public class PlayerGameInfoPanel extends JPanel {
     private Box cardBox;
     private JButton numCards;
     private JLabel title;
-    
-    private int infantryNum = 0;
-    private int cavalryNum = 0;
-    private int artillery = 0;
-    
+
+    private int armyNum = 0;
+
+
     /**
      * Constructor
+     *
      * @param currentPlayer player that has the turn
-     * @throws IOException 
+     * @throws IOException
      */
     public PlayerGameInfoPanel(PlayerModel currentPlayer) throws IOException {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.setSize(150,450);
+        this.setSize(150, 450);
         this.playerName = new JButton();
-        
-        this.numArmies = new JButton("Army Owned: " + currentPlayer.getNumArmiesOwned()); //I think it is going to be hard keep track of this
+
+        this.numArmies = new JButton("Army Owned: " + currentPlayer.getNumArmiesOwned());
         this.armyBox = new Box(BoxLayout.X_AXIS);
         this.numContinents = new JButton("Continent Owned: " + currentPlayer.getContinentsOwned().size());
         this.numCountries = new JButton("Country Owned: " + currentPlayer.getContriesOwned().size());
         this.numCards = new JButton("Card Owned: " + currentPlayer.getCardsOwned().getHand().size());
         this.cardBox = new Box(BoxLayout.X_AXIS);
-        
-        //I think you are repeating code, this should be eliminated
-        /*
-        // initialize the armyBox
-        ImageIcon infantryIcon = new ImageIcon("." + File.separator + "images" + File.separator + "icon-1.png");
-        ImageIcon cavalryIcon = new ImageIcon("." + File.separator + "images" + File.separator + "icon-2.png");
-        ImageIcon artilleryIcon = new ImageIcon("." + File.separator + "images" + File.separator + "icon-3.png");
-        
-        JLabel numInfantry = new JLabel(infantryIcon);
-        JLabel numCavalry = new JLabel(cavalryIcon);
-        JLabel numArtillery = new JLabel(artilleryIcon);
-        
-        Box artilleryArmyBox = new Box(BoxLayout.X_AXIS);
-        int totalNumArmies = currentPlayer.getNumArmies();  // get total amount of armies owned
-        this.artillery = totalNumArmies / 10;
-        artilleryArmyBox.add(numArtillery);
-        artilleryArmyBox.add(new JLabel("* " + artillery + "     "));
-        
-        this.cavalryNum = (totalNumArmies - (totalNumArmies / 10) * 10) / 5;
-        Box cavalryArmyBox = new Box(BoxLayout.X_AXIS);
-        cavalryArmyBox.add(numCavalry);
-        cavalryArmyBox.add(new JLabel("* " + cavalryNum + "     "));
-        
-        this.infantryNum = totalNumArmies - (totalNumArmies / 10) * 10 - (((totalNumArmies - (totalNumArmies / 10) * 10) / 5) * 5);
-        Box infantryArmyBox = new Box(BoxLayout.X_AXIS);
-        infantryArmyBox.add(numInfantry);
-        infantryArmyBox.add(new JLabel("* " + infantryNum + "     "));
-        this.armyBox.add(infantryArmyBox);
-        this.armyBox.add(cavalryArmyBox);
-        this.armyBox.add(artilleryArmyBox);
-        
-        // initialize the cardBox
-        Box infantryCardBox = new Box(BoxLayout.Y_AXIS);
-        Box cavalryCardBox = new Box(BoxLayout.Y_AXIS);
-        Box artilleryCardBox = new Box(BoxLayout.Y_AXIS);
-        for (CardModel hand : this.currentPlayer.getCardsOwned().getHand()) {
-            // get the corresponding card ImageIcon and resize the card image
-            ImageIcon cardIcon = new ImageIcon("." + File.separator + "images" 
-                    + File.separator + hand.getTypeOfArmie() + ".png");
-            Image image = cardIcon.getImage();
-            Image newImage = image.getScaledInstance(50, 70, java.awt.Image.SCALE_SMOOTH);
-            cardIcon = new ImageIcon(newImage);
 
-
-            JButton aux = new JButton();
-            aux.setIcon(cardIcon);
-            aux.setText("");
-            switch(hand.getTypeOfArmie()) {
-                case "infantry":
-                    infantryCardBox.add(aux);
-                    break;
-                case "cavalry":
-                    cavalryCardBox.add(aux);
-                    break;
-                case "artillery":
-                    artilleryCardBox.add(aux);
-                    break;
-            }
-        }
-        cardBox.add(infantryCardBox);
-        cardBox.add(cavalryCardBox);
-        cardBox.add(artilleryCardBox); */
-        
-        // add player name
         this.add(this.playerName);
         this.add(this.numArmies);
         this.add(this.armyBox);
@@ -126,66 +64,37 @@ public class PlayerGameInfoPanel extends JPanel {
         this.add(this.numContinents);
         this.add(this.numCards);
         this.add(this.cardBox);
-        
-        updatePlayer(currentPlayer); //It is better if you call it from outside but it was just to reuse the code
-        // work:
-        /*
-        1. make the player panel more beautiful (title, border, card image, fix the width)
-        2. 
-        */
-        // set a background
-//        try {
-//            image = ImageIO.read(new File("." + File.separator + "images" 
-//                    + File.separator + "PlayerPanelBackground.jpg"));
-//        } catch (IOException ex) {
-//            System.out.println("playerPanelBackground.jpg not found.");
-//        }
-//        background = new JLabel(new ImageIcon(image));
-//        this.add(background);
-        
-        // 
+
+        updatePlayer(currentPlayer);
     }
-    
+
     /**
-     * 
-     * @param currentPlayer 
+     *
+     * @param currentPlayer
      */
-    public void updatePlayer(PlayerModel currentPlayer){        
+    public void updatePlayer(PlayerModel currentPlayer) {
         // update player name and color
         playerName.setText(currentPlayer.getName());
         playerName.setBackground(currentPlayer.getColor());
-        
+
         // update armies
-        ImageIcon infantryIcon = new ImageIcon("." + File.separator + "images" + File.separator + "icon-1.png");
-        ImageIcon cavalryIcon = new ImageIcon("." + File.separator + "images" + File.separator + "icon-2.png");
-        ImageIcon artilleryIcon = new ImageIcon("." + File.separator + "images" + File.separator + "icon-3.png");
-        JLabel numInfantry = new JLabel(infantryIcon);
-        JLabel numCavalry = new JLabel(cavalryIcon);
-        JLabel numArtillery = new JLabel(artilleryIcon);
-        Box artilleryArmyBox = new Box(BoxLayout.X_AXIS);
-        int totalNumArmies = currentPlayer.getNumArmiesOwned();  // get total amount of armies owned
-        this.artillery = totalNumArmies / 10;
-        artilleryArmyBox.add(numArtillery);
-        artilleryArmyBox.add(new JLabel("* " + artillery + "     "));
-        this.cavalryNum = (totalNumArmies - (totalNumArmies / 10) * 10) / 5;
-        Box cavalryArmyBox = new Box(BoxLayout.X_AXIS);
-        cavalryArmyBox.add(numCavalry);
-        cavalryArmyBox.add(new JLabel("* " + cavalryNum + "     "));
-        this.infantryNum = totalNumArmies - (totalNumArmies / 10) * 10 - (((totalNumArmies - (totalNumArmies / 10) * 10) / 5) * 5);
-        Box infantryArmyBox = new Box(BoxLayout.X_AXIS);
-        infantryArmyBox.add(numInfantry);
-        infantryArmyBox.add(new JLabel("* " + infantryNum + "     "));
-        this.armyBox.removeAll();
-        this.armyBox.add(infantryArmyBox);
-        this.armyBox.add(cavalryArmyBox);
-        this.armyBox.add(artilleryArmyBox);
+        ImageIcon armyIcon = new ImageIcon("." + File.separator + "images" + File.separator + "icon-1.png");
+        JLabel numArmies = new JLabel(armyIcon);
+        Box armyBox = new Box(BoxLayout.X_AXIS);
+        this.armyNum = currentPlayer.getNumArmiesOwned();  // get total amount of armies owned
         
+        this.armyBox.removeAll();
+        this.armyBox.add(numArmies);
+        this.armyBox.add(new JLabel("* " + armyNum + "     "));
+
+                
+
         // update countries and continents
         this.numCountries.setText("Countries Owned: " + Integer.toString(currentPlayer
                 .getContriesOwned().size()));
         this.numContinents.setText("Continents Owned: " + Integer.toString(currentPlayer
                 .getContinentsOwned().size()));
-        
+
         // update cards
         this.numCards.setText("Card Owned: " + currentPlayer.getCardsOwned().getHand().size());
         Box infantryCardBox = new Box(BoxLayout.Y_AXIS);
@@ -193,7 +102,7 @@ public class PlayerGameInfoPanel extends JPanel {
         Box artilleryCardBox = new Box(BoxLayout.Y_AXIS);
         for (CardModel hand : currentPlayer.getCardsOwned().getHand()) {
             // get the corresponding card ImageIcon and resize the card image
-            ImageIcon cardIcon = new ImageIcon("." + File.separator + "images" 
+            ImageIcon cardIcon = new ImageIcon("." + File.separator + "images"
                     + File.separator + hand.getTypeOfArmie() + ".png");
             Image image = cardIcon.getImage();
             Image newImage = image.getScaledInstance(50, 70, java.awt.Image.SCALE_SMOOTH);
@@ -202,7 +111,7 @@ public class PlayerGameInfoPanel extends JPanel {
             aux.setIcon(cardIcon);
             aux.setText("");
             aux.setBackground(currentPlayer.getColor());
-            switch(hand.getTypeOfArmie()) {
+            switch (hand.getTypeOfArmie()) {
                 case "infantry":
                     infantryCardBox.add(aux);
                     break;
@@ -218,8 +127,7 @@ public class PlayerGameInfoPanel extends JPanel {
         cardBox.add(infantryCardBox);
         cardBox.add(cavalryCardBox);
         cardBox.add(artilleryCardBox);
-        
-        
+
     }
 
     /**
