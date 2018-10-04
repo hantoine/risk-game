b39a8e941822e30f4bd7ec5f6fe576bb37539464
Timothée Guédon <ava_punksmash@protonmail.com>
2018-10-04 18:17:10 -0400

    add first selection

--- riskGame/src/com/risk/mapeditor/ToolsListPanel.java
+++ riskGame/src/com/risk/mapeditor/ToolsListPanel.java
@@ -6,6 +6,11 @@
 package com.risk.mapeditor;
 
 import com.risk.mapeditor.MapEditorPanel.ToolButtonListener;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 import javax.swing.JButton;
 
 /**
@@ -20,7 +25,31 @@ public class ToolsListPanel extends CustomListPanel {
         for(String toolName:toolsList){
             JButton newTool = new JButton(toolName);
             newTool.addActionListener(buttonListener);
+            newTool.addActionListener(new selectButtonListener(this));
             this.addElement(newTool, toolName);
+        }
+    }
+    
+    protected class selectButtonListener implements ActionListener{
+
+        private final ToolsListPanel panel;
+
+        private selectButtonListener(ToolsListPanel panel) {
+            this.panel = panel;
+        }
+                
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JButton target = (JButton)e.getSource();
+            
+            HashMap<String, Component> currentmap = this.panel.items;
+            for(HashMap.Entry<String, Component> buttonEntry: currentmap.entrySet()){
+                JButton button =(JButton)buttonEntry.getValue();
+                if(target.getText().equals(button.getText()))
+                    button.setBackground(Color.cyan);
+                else
+                    button.setBackground(Color.white);
+            }
         }
     }
 }
