beea8194a01756e698084caff6474cb4bce0c2f7
Antoine Hébert <hantoine02@gmail.com>
2018-10-06 14:42:10 -0400

    Change the access modifier of some of the methods of RiskView that were public to no modifier. This way they are no longer accessible outside the view. This ensure a strong seperation between the view and the other modules and make it simpler to develop without linking these modules to much

--- riskGame/src/com/risk/controllers/MenuListener.java
+++ riskGame/src/com/risk/controllers/MenuListener.java
@@ -107,7 +107,7 @@ public class MenuListener extends MouseAdapter {
                 }
                 break;
             case "PLAY":
-                NewGamePanel newGamePanel = this.getRiskView().getMenuPanel().getStartMenu().getNewGamePanel();
+                NewGamePanel newGamePanel = this.getRiskView().getNewGamePanel();
                 String selectedPath = newGamePanel.getSelectFileTextField().getText();
 
                 if (selectedPath.equals("")) {
@@ -126,10 +126,7 @@ public class MenuListener extends MouseAdapter {
                     break;
                 }
 
-                this.getRiskView().getMenuPanel().setVisible(false);
-                this.getRiskView().remove(this.getRiskView().getMenuPanel());
-                this.getRiskView().hideMenu();
-                this.getRiskView().getStagePanel().setVisible(true);
+                this.getRiskView().closeMenu();
 
                 LinkedList<PlayerPanel> listPlayerPanels = newGamePanel.getPlayersPanel().getPlayersArray();
                 LinkedList<PlayerModel> listPlayers = new LinkedList<>();
