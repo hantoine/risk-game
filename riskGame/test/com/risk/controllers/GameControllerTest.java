d6c0d8747f91b7d6af0f8833bda7d1b746642b48
liyixuan <liyixuan4030614@gmail.com>
2018-10-27 13:07:39 -0400

    Create test for DragNDropTerritory

--- riskGame/test/com/risk/controllers/GameControllerTest.java
+++ riskGame/test/com/risk/controllers/GameControllerTest.java
@@ -5,8 +5,18 @@
  */
 package com.risk.controllers;
 
-import org.junit.After;
-import static org.junit.Assert.fail;
+import com.risk.models.ContinentModel;
+import com.risk.models.GamePhase;
+import com.risk.models.MapModel;
+import com.risk.models.PlayerModel;
+import com.risk.models.RiskModel;
+import com.risk.models.TerritoryModel;
+import com.risk.views.RiskViewInterface;
+import com.risk.views.menu.NewGamePanel;
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.LinkedList;
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,26 +26,121 @@ import org.junit.Test;
  */
 public class GameControllerTest {
 
+    private GameController instance;
+    private RiskModel rm;
+
     @Before
     public void setUp() {
+        rm = new RiskModel();
+        rm.removePlayer(0);
+        rm.removePlayer(0);
+        rm.removePlayer(0);
+        rm.addPlayerToPlayerList("PlayerA", Color.yellow, true);
+        rm.addPlayerToPlayerList("PlayerB", Color.red, true);
+        rm.setMap(getTestMap());
+        DummyRiskView drv = new DummyRiskView();
+        instance = new GameController(rm, drv);
     }
 
-    @After
-    public void tearDown() {
+    private static MapModel getTestMap() {
+        MapModel map = new MapModel();
+
+        HashMap<String, ContinentModel> graphContinents = new HashMap<>();
+        String name;
+        name = "ContinentA";
+        graphContinents.put(name, new ContinentModel(name, 3));
+
+        HashMap<String, TerritoryModel> graphTerritories = new HashMap<>();
+        name = "TerritoryA";
+        graphTerritories.put(name, new TerritoryModel(name, 50, 50));
+        name = "TerritoryB";
+        graphTerritories.put(name, new TerritoryModel(name, 100, 50));
+
+        LinkedList<TerritoryModel> memberTerritories = new LinkedList();
+        graphTerritories.get("TerritoryA").setContinentName("ContinentA");
+        memberTerritories.add(graphTerritories.get("TerritoryA"));
+        graphTerritories.get("TerritoryB").setContinentName("ContinentA");
+        memberTerritories.add(graphTerritories.get("TerritoryB"));
+        graphContinents.get("ContinentA").setMembers(memberTerritories);
+
+        LinkedList<TerritoryModel> adjacentTerritories = new LinkedList<>();
+        adjacentTerritories.add(graphTerritories.get("TerritoryB"));
+        graphTerritories.get("TerritoryA").setAdj(adjacentTerritories);
+
+        adjacentTerritories = new LinkedList<>();
+        adjacentTerritories.add(graphTerritories.get("TerritoryA"));
+        graphTerritories.get("TerritoryB").setAdj(adjacentTerritories);
+
+        map.setGraphTerritories(graphTerritories);
+        map.setGraphContinents(graphContinents);
+
+        return map;
     }
 
     /**
-     * Test of dragNDropTerritory method, of class GameController.
+     * Test of dragNDropTerritory method, of class GameController, in
+     * fortification stage when everything is fine
      */
     @Test
     public void testDragNDropTerritory() {
         System.out.println("dragNDropTerritory");
-        String sourceTerritoryName = "";
-        String destTerritoryName = "";
-        GameController instance = null;
+        String sourceTerritoryName = "TerritoryA";
+        String destTerritoryName = "TerritoryB";
+        TerritoryModel territoryA = rm.getMap().getGraphTerritories().get(sourceTerritoryName);
+        TerritoryModel territoryB = rm.getMap().getGraphTerritories().get(destTerritoryName);
+
+        rm.setStage(GamePhase.FORTIFICATION);
+        rm.setCurrentPlayer(rm.getPlayerList().getFirst());
+        PlayerModel playerA = rm.getPlayerList().getFirst();
+        playerA.addCountryOwned(territoryA);
+        playerA.addCountryOwned(territoryB);
+        territoryA.setNumArmies(2);
+        territoryB.setNumArmies(1);
+
         instance.dragNDropTerritory(sourceTerritoryName, destTerritoryName);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        assertEquals(territoryA.getNumArmies(), 1);
+        assertEquals(territoryB.getNumArmies(), 2);
+    }
+
+    private static class DummyRiskView implements RiskViewInterface {
+
+        public DummyRiskView() {
+        }
+
+        @Override
+        public void updateView(RiskModel rm) {
+        }
+
+        @Override
+        public void updateViewWithNewMap(RiskModel rm) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void showMessage(String message) {
+            System.out.println(message);
+        }
+
+        @Override
+        public void setController(RiskController rc) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void initialMenu(RiskModel riskModel, MenuListener menuListener) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void closeMenu() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public NewGamePanel getNewGamePanel() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
     }
 
 }
