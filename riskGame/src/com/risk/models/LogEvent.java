b5ce5a7f90d40e3ded309effb4444e9777a3f85c
Antoine Hébert <hantoine02@gmail.com>
2018-11-03 19:52:15 -0400

    Clear log when new game starts

--- riskGame/src/com/risk/models/LogEvent.java
+++ riskGame/src/com/risk/models/LogEvent.java
@@ -12,12 +12,23 @@ package com.risk.models;
 public class LogEvent {
 
     String message;
+    boolean clear;
 
     LogEvent(String message) {
         this.message = message;
     }
 
+    LogEvent(String message, boolean clear) {
+        this(message);
+        this.clear = true;
+    }
+
+    @Override
     public String toString() {
         return message;
+    }
+
+    public boolean isClear() {
+        return clear;
     }
 }
