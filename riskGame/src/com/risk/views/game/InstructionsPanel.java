dc884c08ab5debf876c0cc0191964fbbf6d3e167
liyixuan <liyixuan4030614@gmail.com>
2018-11-22 23:56:56 -0500

    Update InstructionPanel to use the new LogModel

--- riskGame/src/com/risk/views/game/InstructionsPanel.java
+++ riskGame/src/com/risk/views/game/InstructionsPanel.java
@@ -5,6 +5,7 @@
  */
 package com.risk.views.game;
 
+import com.risk.models.HumanStrategy;
 import com.risk.models.PlayerModel;
 import com.risk.models.RiskModel;
 import java.util.Observable;
@@ -62,7 +63,7 @@ public class InstructionsPanel extends JPanel implements Observer {
 
         switch (rm.getPhase()) {
             case STARTUP:
-                this.endPhase.setVisible(true);
+                this.endPhase.setVisible(true); //rm.getCurrentPlayer().getStrategy() instanceof HumanStrategy
                 this.endPhase.setText("Place armies for all players");
                 this.text.setText(String.format("<html>Startup phase: Click on one of your territory to place an army on it. <br />Remaining armies to be placed: %d</html>",
                         currentPlayer.getNbArmiesAvailable()));
@@ -75,13 +76,13 @@ public class InstructionsPanel extends JPanel implements Observer {
                 this.text.setVisible(true);
                 break;
             case ATTACK:
-                this.endPhase.setVisible(true);
+                this.endPhase.setVisible(rm.getCurrentPlayer().getStrategy() instanceof HumanStrategy);
                 this.endPhase.setText("End Phase");
                 this.text.setText("<html>Attack phase: You can drag'n'drop between a territory you own <br /> and a territory you do not own.</html>");
                 this.text.setVisible(true);
                 break;
             case FORTIFICATION:
-                this.endPhase.setVisible(true);
+                this.endPhase.setVisible(rm.getCurrentPlayer().getStrategy() instanceof HumanStrategy);
                 this.endPhase.setText("End Phase");
                 this.text.setText("<html>Fortification phase: You can drag'n'drop between territories you own. <br />Move your armies <br /></html>");
                 this.text.setVisible(true);
